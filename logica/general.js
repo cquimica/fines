715b2e9f741f75e0fba0edb3e0bf3e75e27b5047
Haader <37Hader38@gmail.com>
2022-08-31 08:15:44 -0300

    agregando links

--- logica/general.js
+++ logica/general.js
@@ -4,9 +4,7 @@ function leer(archivo){
     <div class="visorPDF" id="visorPDF">
         <button class="btnExit" onclick="Exit()"><ion-icon name="close-circle-outline"></ion-icon></button>
         
-        
-        <embed src="material/${archivo}.pdf" type="application/pdf" width="100%" height="100%" />
-
+        <iframe src="https://docs.google.com/viewer?srcid=${archivo}&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="100%" height="100%"></iframe>
 
     </div>
 
@@ -21,12 +19,37 @@ function Exit(){
 
 /*
 copiar
-<iframe src="https://docs.google.com/viewer?srcid=1OEe5kHKB-olY1mjkpiyHfbi5aMZkVsrL&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="100%" height="680px"></iframe>
-
-
-<object data="material/${archivo}.pdf" height="100%" width="100%"></object>
-
-
-
+<iframe src="https://docs.google.com/viewer?srcid=${archivo}&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="100%" height="680px"></iframe>
 
 */
+
+/*
+modulo:
+1Zqhu7NJrwfSfkEryS3_jkS9P_jW9Fq26
+
+actividades:
+1Ss7k_ylJf0FlQNrJ_dPB300MfOZyUsqT
+
+terminos:
+1CcNY5RGmAlNzWoNKNi1K0l_Hk_K2Yu1F
+
+t1:
+1LZID22ouYDeoOn5xubnEDYfaRBsUIzoQ
+
+t2:
+1iXbPGGpcvgINjKfx3lpvFQgRYBX1IvaY
+
+t3:
+
+t4:
+1uTsUU4qgZtHQOBi0a33aw5BavtDgqpPF
+
+t5:
+1WEZdvHyFl9t9C-kiFGUHQiizZhpSyRs8
+
+t6:
+13IWHzwfoXf2KYXbGwnZJkaiIDfI_njLA
+
+t7:
+17lKcmggzDEye3QKT9xIra6SYuVgLgeDJ
+*/ 
