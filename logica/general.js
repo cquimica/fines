bb0ffd0da71d4badab764a192f16aaa8a3d8e8a9
Haader <37Hader38@gmail.com>
2022-08-30 23:49:29 -0300

    def

--- logica/general.js
+++ logica/general.js
@@ -6,16 +6,18 @@ function leer(archivo){
         
         <object data="material/${archivo}.pdf" height="100%" width="100%"></object>
             
-        <iframe src="http://docs.google.com/gview?  url=http://cquimica.github.io/fines/material/${archivo}.pdf&embedded=true" style="width:500px; height:375px;" frameborder="0"></iframe>
+        <iframe src="http://docs.google.com/gview?  url=http://cquimica.github.io/fines/material/${archivo}.pdf&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>
 
 
     </div>
 
     `;
+    
 }
 
 function Exit(){
     document.getElementById("visorPDF").remove();
+    alert("funciona?");
 }
 
 /*
