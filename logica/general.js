58e305e4419a31ddd2b8b52a6028175dd86c07a0
Haader <37Hader38@gmail.com>
2022-08-30 23:38:08 -0300

    usando object

--- logica/general.js
+++ logica/general.js
@@ -3,7 +3,7 @@ function leer(archivo){
     
     <div class="visorPDF" id="visorPDF">
         <button class="btnExit" onclick="Exit()"><ion-icon name="close-circle-outline"></ion-icon></button>
-        <iframe src="material/${archivo}.pdf" height="100%" width="100%"></iframe>
+        <object data="material/${archivo}.pdf" height="100%" width="100%"></object>
     </div>
 
     `;
