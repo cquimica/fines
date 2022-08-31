0bc5c089d9bd4210549a0eeb38665ccdfd0f6d8d
Haader <37Hader38@gmail.com>
2022-08-31 00:19:57 -0300

    funciona?

--- logica/general.js
+++ logica/general.js
@@ -5,7 +5,7 @@ function leer(archivo){
         <button class="btnExit" onclick="Exit()"><ion-icon name="close-circle-outline"></ion-icon></button>
         
         
-        <iframe src="http://docs.google.com/gview?  url=http://cquimica.github.io/fines/material/${archivo}.pdf" style="width:100%; height:100%;" frameborder="0"></iframe>
+        <iframe src="https://docs.google.com/viewer?srcid=1OEe5kHKB-olY1mjkpiyHfbi5aMZkVsrL&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="100%" height="680px"></iframe>
 
 
     </div>
