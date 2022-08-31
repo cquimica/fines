37241b27fee3a148c8eaf73172b87f4e28ee4359
Haader <37Hader38@gmail.com>
2022-08-31 00:15:53 -0300

    sss

--- logica/general.js
+++ logica/general.js
@@ -4,8 +4,7 @@ function leer(archivo){
     <div class="visorPDF" id="visorPDF">
         <button class="btnExit" onclick="Exit()"><ion-icon name="close-circle-outline"></ion-icon></button>
         
-        <object data="material/${archivo}.pdf" height="100%" width="100%"></object>
-            
+        
         <iframe src="http://docs.google.com/gview?  url=http://cquimica.github.io/fines/material/${archivo}.pdf" style="width:100%; height:100%;" frameborder="0"></iframe>
 
 
@@ -21,7 +20,13 @@ function Exit(){
 }
 
 /*
+copiar
+<iframe src="https://docs.google.com/viewer?srcid=1OEe5kHKB-olY1mjkpiyHfbi5aMZkVsrL&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="100%" height="680px"></iframe>
+
 
 <object data="material/${archivo}.pdf" height="100%" width="100%"></object>
+
+
+
 
 */
